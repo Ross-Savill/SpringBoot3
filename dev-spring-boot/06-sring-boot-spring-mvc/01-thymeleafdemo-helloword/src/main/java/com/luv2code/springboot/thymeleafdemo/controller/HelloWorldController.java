208bb7af907ee114bfc2de4b4cc7c49ca9489284
Ross Savill <rosssavill252@gmail.com>
2026-01-07 00:31:56 +0000

    Finished updating the controller

--- dev-spring-boot/06-sring-boot-spring-mvc/01-thymeleafdemo-helloword/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ dev-spring-boot/06-sring-boot-spring-mvc/01-thymeleafdemo-helloword/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -22,11 +22,24 @@ public class HelloWorldController {
         return "helloworld";
     }
 
+    // need a controller method to read form data and add data to the moddl
+
     @RequestMapping("/processFormVersionTwo")
     public String letsShoutDude(HttpServletRequest request, Model model) {
 
         // read thr request parameter from the HTML form
         String theName = request.getParameter("studentName");
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Yo! " + theName;
+
+        // add message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
 
     }
 
