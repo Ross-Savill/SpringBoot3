cb327ca436c12d75259c7435ba990e17d0dbda2c
Ross Savill <rosssavill252@gmail.com>
2026-01-31 23:24:50 +0000

    Validators added

--- dev-spring-boot/06-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
+++ dev-spring-boot/06-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
@@ -1,8 +1,14 @@
 package com.luv2code.springdemo.mvc;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class Customer {
 
     private String firstName;
+
+    @NotNull(message="is required")
+    @Size(min=1, message="is required")
     private String lastName;
 
     public String getFirstName() {
